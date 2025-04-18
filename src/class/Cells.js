1fb3da619125c4c4a7d5a7ab4ae0ff069419262d
Jan Randa <jnranda@gmail.com>
2025-04-18 14:56:19 +0200

    BF: hasMany.get doesnt pull

--- src/class/Cells.js
+++ src/class/Cells.js
@@ -14,11 +14,20 @@ export class Cells extends Map {
         return c?.isExpired() ? this.reset("expired", id) : c;
     }
 
-    async get(id, ...a) { return this.pick(id)?.get(id, ...a); }
+    async get(id, ...a) {
+        const { _vault } = this;
+        let c = this.pick(id);
+        if (!c) {
+            if (!_vault.remote) { return; }
+            super.set(id, c = new Cell(_vault));
+        }
+        return c.get(id, ...a);
+    }
 
     async set(data, id, ...a) {
+        const { _vault } = this;
         let c = super.get(id);
-        if (!c) { super.set(id, c = new Cell(this._vault)); }
+        if (!c) { super.set(id, c = new Cell(_vault)); }
         return c.set(data, id, ...a);
     }
 
